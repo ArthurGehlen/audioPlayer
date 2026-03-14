d900a0ecb7c5455ee084999c693406bed570bd56
Arthur Gehlen <1arthurgehlen@gmail.com>
2026-03-13 22:27:28 -0300

    Adicionando a barra de progresso

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const timestamp = document.getElementById("timestamp");
+const progress_bar = document.getElementById("progress_bar");
+const container = document.getElementById("container");
 
 const main_music = new Audio();
 main_music.src = "assets/music1.mp3";
@@ -6,28 +8,57 @@ main_music.src = "assets/music1.mp3";
 // não sei oq isso vai virar :)
 
 function get_music_duration() {
-  let raw_duration = main_music.duration; // return the music duration in seconds
+  let raw_duration = main_music.duration; // retorna a duração da música em segundos
+
   let duration_minutes = Math.floor(raw_duration / 60);
   let duration_seconds = Math.floor(raw_duration % 60);
 
-  return `${duration_minutes}:${duration_seconds}` // return formated music duration
+  duration_seconds = duration_seconds.toString().padStart(2, "0");
+
+  return `${duration_minutes}:${duration_seconds}`; // retorna a duração formatada da música :)
 }
-
-main_music.addEventListener("loadedmetadata", () => {
-    let a = get_music_duration();
-    console.log(a)
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    main_music.play();
-    main_music.volume = 0;
-});
 
 function get_audio_current_time() {
   let audio_current_time = Math.floor(main_music.currentTime);
 
-  timestamp.textContent = `${audio_current_time}`;
+  let minutes = Math.floor(audio_current_time / 60);
+  let seconds = Math.floor(audio_current_time % 60);
+
+  seconds = seconds.toString().padStart(2, "0");
+
+  let music_duration = get_music_duration();
+
+  if (timestamp) {
+    timestamp.textContent = `${minutes}:${seconds}/${music_duration}`;
+  }
 }
 
+// atualiza a barra de progresso de acordo com a duração da música
+main_music.addEventListener("timeupdate", () => {
+  const percentage = (main_music.currentTime / main_music.duration) * 100;
+  progress_bar.style.width = percentage + "%";
+});
+
+// calcula a posição do click relativo ao container pai (largura fixa)
+container.addEventListener("click", (e) => {
+  const rect = container.getBoundingClientRect();
+  const percentage = (e.clientX - rect.left) / rect.width;
+  main_music.currentTime = percentage * main_music.duration; // converte em porcentagem e pula para esse ponto do audio
+});
+
+container.addEventListener("click", (e) => {
+  const rect = container.getBoundingClientRect();
+  const percentage = (e.clientX - rect.left) / rect.width;
+  main_music.currentTime = percentage * main_music.duration;
+});
+
+main_music.addEventListener("loadedmetadata", () => {
+  get_audio_current_time();
+});
+
 setInterval(get_audio_current_time, 1000);
-get_audio_current_time();
+
+document.addEventListener("DOMContentLoaded", () => {
+  main_music.play();
+  main_music.volume = 0;
+});
