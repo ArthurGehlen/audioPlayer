12d25321a6888efd0cebab8c9b88ab7986d7b69d
Arthur Gehlen <1arthurgehlen@gmail.com>
2026-03-15 17:31:26 -0300

    Troca da música, mudança no estilo da barra de progresso, adição do botão de pause/play, adição de informações da música

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const timestamp = document.getElementById("timestamp");
+const progress_bar_wrapper = document.getElementById("progress_bar_wrapper");
+const play_btn = document.getElementById("play_btn");
 const progress_bar = document.getElementById("progress_bar");
-const container = document.getElementById("container");
 
-const main_music = new Audio();
-main_music.src = "assets/music1.mp3";
+// futuramente quero adicionar um sistema para o usuário enviar a música
+// por hora eu vou providenciar as músicas
+const CURRENT_MUSIC_PATH = "assets/music1.mp3";
+let current_music_state = "pause";
 
-// não sei oq isso vai virar :)
+const music_audio = new Audio();
+music_audio.src = CURRENT_MUSIC_PATH;
+
+// TODO: botão de replay
+// TODO: adicionar mais músicas (json?)
 
 function get_music_duration() {
-  let raw_duration = main_music.duration; // retorna a duração da música em segundos
+  let raw_duration = music_audio.duration; // retorna a duração da música em segundos
 
   let duration_minutes = Math.floor(raw_duration / 60);
   let duration_seconds = Math.floor(raw_duration % 60);
@@ -19,7 +26,7 @@ function get_music_duration() {
 }
 
 function get_audio_current_time() {
-  let audio_current_time = Math.floor(main_music.currentTime);
+  let audio_current_time = Math.floor(music_audio.currentTime);
 
   let minutes = Math.floor(audio_current_time / 60);
   let seconds = Math.floor(audio_current_time % 60);
@@ -33,32 +40,37 @@ function get_audio_current_time() {
   }
 }
 
+function change_music_state() {}
+
 // atualiza a barra de progresso de acordo com a duração da música
-main_music.addEventListener("timeupdate", () => {
-  const percentage = (main_music.currentTime / main_music.duration) * 100;
+music_audio.addEventListener("timeupdate", () => {
+  const percentage = (music_audio.currentTime / music_audio.duration) * 100;
   progress_bar.style.width = percentage + "%";
 });
 
 // calcula a posição do click relativo ao container pai (largura fixa)
-container.addEventListener("click", (e) => {
-  const rect = container.getBoundingClientRect();
+progress_bar_wrapper.addEventListener("click", (e) => {
+  const rect = progress_bar_wrapper.getBoundingClientRect();
   const percentage = (e.clientX - rect.left) / rect.width;
-  main_music.currentTime = percentage * main_music.duration; // converte em porcentagem e pula para esse ponto do audio
+  music_audio.currentTime = percentage * music_audio.duration; // converte em porcentagem e pula para esse ponto do audio
 });
 
-container.addEventListener("click", (e) => {
-  const rect = container.getBoundingClientRect();
-  const percentage = (e.clientX - rect.left) / rect.width;
-  main_music.currentTime = percentage * main_music.duration;
-});
-
-main_music.addEventListener("loadedmetadata", () => {
+music_audio.addEventListener("loadedmetadata", () => {
   get_audio_current_time();
 });
 
 setInterval(get_audio_current_time, 1000);
 
-document.addEventListener("DOMContentLoaded", () => {
-  main_music.play();
-  main_music.volume = 0;
+// tive problemas com a política de reprodução automática do chrome
+// então a música só vai começar a tocar quando clicar na página
+play_btn.addEventListener("click", () => {
+  if (current_music_state == "pause") {
+    play_btn.innerHTML = `<i class="fa-solid fa-pause" style="color: rgb(255, 255, 255);"></i>`;
+    music_audio.play();
+    current_music_state = "play";
+  } else {
+    play_btn.innerHTML = `<i class="fa-solid fa-play" style="color: rgb(255, 255, 255);"></i>`;
+    music_audio.pause();
+    current_music_state = "pause";
+  }
 });
